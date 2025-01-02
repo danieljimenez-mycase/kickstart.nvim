f97e5571f7a32b4a6fff206149d4d54ee69b915c
danieljimenez-mycase <daniel.jimenez@mycase.com>
2025-01-01 18:43:06 -0800

    fixup! Change colorscheme to gruvbox

--- init.lua
+++ init.lua
@@ -849,6 +849,9 @@ require('lazy').setup({
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
+    opts = {
+      transparent_mode = true,
+    },
   },
 
   -- Highlight todo, notes, etc in comments
