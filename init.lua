f5ac3516b454da1627bd4aa25bb2357c0214cedf
danieljimenez-mycase <daniel.jimenez@mycase.com>
2025-01-02 20:24:04 -0800

    Add copy file path keybinding

--- init.lua
+++ init.lua
@@ -190,6 +190,12 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+local function copyFilePath()
+  local filepath = vim.fn.expand '%'
+  vim.fn.setreg('+', filepath)
+end
+
+vim.keymap.set('n', '<leader>cp', copyFilePath, { noremap = true, silent = true, desc = 'copy file path' })
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
