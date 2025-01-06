709e8ae81c79317ea964a891cb60ca6f70e4182f
danieljimenez-mycase <daniel.jimenez@mycase.com>
2025-01-05 17:45:53 -0800

    Add smear cursor

--- init.lua
+++ init.lua
@@ -891,6 +891,13 @@ require('lazy').setup({
     },
   },
 
+  {
+    'sphamba/smear-cursor.nvim',
+    opts = {
+      smear_between_neighbor_lines = true,
+    },
+  },
+
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
