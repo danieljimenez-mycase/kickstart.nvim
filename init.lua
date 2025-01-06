30a2260b92fddb0f69c0e8ce19b84af37144c2f0
danieljimenez-mycase <daniel.jimenez@mycase.com>
2025-01-06 09:06:03 -0800

    Add mini bracketed

--- init.lua
+++ init.lua
@@ -838,6 +838,23 @@ require('lazy').setup({
       end
 
       require('mini.splitjoin').setup()
+
+      require('mini.bracketed').setup {
+        buffer = { suffix = 'b', options = {} },
+        comment = { suffix = 'm', options = {} },
+        conflict = { suffix = 'x', options = {} },
+        diagnostic = { suffix = 'd', options = {} },
+        file = { suffix = 'f', options = {} },
+        indent = { suffix = 'i', options = {} },
+        jump = { suffix = 'j', options = {} },
+        location = { suffix = 'l', options = {} },
+        oldfile = { suffix = 'o', options = {} },
+        quickfix = { suffix = 'q', options = {} },
+        treesitter = { suffix = 't', options = {} },
+        undo = { suffix = 'u', options = {} },
+        window = { suffix = 'w', options = {} },
+        yank = { suffix = 'y', options = {} },
+      }
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
